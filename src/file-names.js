416303dd1e9eced3e299f48ded0a43a934a4e2f1
barodzich aliaksandr <barodzichaliaksandr@gmail.com>
2025-01-05 21:29:46 +0300

    feat: implement renameFiles fn

--- src/file-names.js
+++ src/file-names.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * There's a list of file, since two files cannot have equal names,
  * the one which comes later will have a suffix (k),
@@ -15,9 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  const existFileNames = new Map();
+
+  for (const name of names) {
+    const amount = existFileNames.get(name);
+    if (amount >= 1) {
+      const newName = name.concat(`(${amount})`);
+      existFileNames.set(newName, 1);
+    }
+    existFileNames.set(name, amount === undefined ? 1 : amount + 1);
+  }
+
+  return Array.from(existFileNames.keys());
 }
 
 module.exports = {
