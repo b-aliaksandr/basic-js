1ac964d64c3f754c2d8aa0741b2830ad854f6141
barodzich aliaksandr <barodzichaliaksandr@gmail.com>
2025-01-06 13:22:49 +0300

    feat: implement minesweeper fn

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
  * that don't contain a mine have a number in it that indicates the total number of mines
@@ -23,9 +21,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const rowLen = matrix.length;
+  const colLen = matrix.at(0)?.length || 0;
+
+  const result = (Array.from({ length: rowLen }, () => Array(colLen).fill(0)));
+
+  const incNeighborsTotal = (row, col) => {
+    const [minRow, maxRow] = [row === 0 ? row : row - 1, row === rowLen - 1 ? row : row + 1];
+    const [minCol, maxCol] = [col === 0 ? col : col - 1, col === colLen - 1 ? col : col + 1];
+
+    for (let rowIndex = minRow; rowIndex <= maxRow; rowIndex += 1) {
+      for (let colIndex = minCol; colIndex <= maxCol; colIndex += 1) {
+        if (row === rowIndex && col === colIndex) continue;
+        const prevTotal = result[rowIndex][colIndex];
+        result[rowIndex][colIndex] = prevTotal + 1;
+      }
+    }
+  };
+
+  for (let rowIndex = 0; rowIndex < rowLen; rowIndex += 1) {
+    for (let colIndex = 0; colIndex < colLen; colIndex += 1) {
+      const isMine = matrix[rowIndex][colIndex];
+      if (isMine) {
+        incNeighborsTotal(rowIndex, colIndex);
+      }
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
